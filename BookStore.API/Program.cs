a5e589a86675ba1ed22fd1e93e78baa738c15bde
David Cronier <david.r.cronier@gmail.com>
2024-10-20 11:02:48 -0400

    Initial Commit

---
+++ BookStore.API/Program.cs
@@ -0,0 +1,50 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "DevCorsPolicy", policy =>
+    {
+        policy.WithOrigins(
+            "https://localhost:1111",
+            "http://localhost:1111"
+            )
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseCors("DevCorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
